85706498bc033eadbe54d905a05a9fa3a5b9996a
Bilal Arif <bilaljamoot1995@gmail.com>
2020-06-18 15:04:28 +0500

    Fetch Data from API function implemented and video completed till 23:30

--- src/App.js
+++ src/App.js
@@ -3,8 +3,18 @@ import React from 'react';
 import { Cards, Chart, CountryPicker } from './components';
 
 import styles from './App.module.css'
-
+// Goal of using index file in every component is that, we don't have to use the filename it will automatically catch the index file inside component folder
+import { fetchData } from './api';
+ 
 class App extends React.Component {
+
+ async componentDidMount() {
+        const data = await fetchData();
+        console.log(data);
+    }
+
+
+
     render() {
         return(
             <div className={styles.container}>
